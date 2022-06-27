07f48f4916b88bb94c79d9553923c51137351d15
Priyadarshini Karanth <79926717+priyak1203@users.noreply.github.com>
2022-06-27 23:01:55 +0530

    Lorem Ipsum Project

---
+++ 08-lorem-ipsum/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import data from './data';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    // if (count <= 0) {
+    //   amount = 1;
+    // }
+    // if (count > data.length) {
+    //   amount = data.length;
+    // }
+    // setText(data.slice(0, amount));
+
+    setText([]);
+    if (count <= 0) {
+      alert('Please choose a number greater than 0');
+    } else if (count > data.length) {
+      alert(`Please choose a number less than ${data.length}`);
+    } else {
+      setText(data.slice(0, amount));
+    }
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
